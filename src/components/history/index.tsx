bc3594f2ebd8883887f8a02cda34d847ad7155b7
anthonymin <dbstnsdl12@naver.com>
2020-09-03 02:28:57 +0900

    Design History log list page

--- src/components/history/index.tsx
+++ src/components/history/index.tsx
@@ -1,15 +1,52 @@
 import React from 'react';
-import { Box, makeStyles } from '@material-ui/core';
+import {
+    Avatar,
+    Box,
+    List, 
+    ListItem,
+    ListItemAvatar,
+    ListItemText,
+} from '@material-ui/core';
+import HowToRegIcon from '@material-ui/icons/HowToReg';
+import VpnKeyIcon from '@material-ui/icons/VpnKey';
 
-const useStyles = makeStyles(() => ({
-    // TODO
-}));
+type History = {
+    type: string,
+    createdAt: string,
+    origin: string,
+};
 
-export default function HistoryListPage () {
+type HistoryListPageProps = {
+    histories: Array<History>
+};
+
+export default function HistoryListPage ({ histories }: HistoryListPageProps){
     
     return (
         <Box>
-            history
+            <List>
+                {
+                    histories.map((history) => {
+                        return (
+                            <ListItem divider>
+                                <ListItemAvatar>
+                                    <Avatar>
+                                        {
+                                            history.type === 'REGISTRATION'
+                                                ? <HowToRegIcon />
+                                                : <VpnKeyIcon />
+                                        }
+                                    </Avatar>
+                                </ListItemAvatar>
+                                <ListItemText 
+                                    primary={history.type} 
+                                    secondary={`${new Date(history.createdAt).toDateString()}  ${history.origin}`} 
+                                />
+                            </ListItem>
+                        )
+                    })
+                }
+            </List>
         </Box>
     );
 };
